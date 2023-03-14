18a8f80df95606cf6c4ea29818ec0d08fbeadc6c
hennneh <hendrik.lehmann96@gmail.com>
2023-03-14 18:07:43 +0100

    adjust comments to norm

--- C/Colleene.c
+++ C/Colleene.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-/*c1*/
-char*s="#include<stdio.h>%c/*c1*/%cchar*s=%c%s%c;%cvoid quine(){printf(s,10,10,34,s,34,10,10,10);}%cint main(){/*c2*/quine();}%c";
-void quine(){printf(s,10,10,34,s,34,10,10,10);}
-int main(){/*c2*/quine();}
+/*
+	a comment
+*/
+char*s="#include<stdio.h>%c/*%c	a comment%c*/%cchar*s=%c%s%c;%cvoid quine(){printf(s,10,10,10,10,34,s,34,10,10,10,10,10,10,10,10,10);}%cint main()%c{%c/*%c	another comment%c*/%cquine();%c}%c";
+void quine(){printf(s,10,10,10,10,34,s,34,10,10,10,10,10,10,10,10,10);}
+int main()
+{
+/*
+	another comment
+*/
+quine();
+}
