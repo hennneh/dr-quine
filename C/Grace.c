b8e4dfb07c1c23341069882e15ae1c37a18ffe1b
hennneh <hendrik.lehmann96@gmail.com>
2023-03-14 14:52:20 +0100

    adding Grace written in C

---
+++ C/Grace.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#define STRING "#include<stdio.h>%c#define STRING %c%s%c%c#define MAIN() GRACE(STRING)%c#define GRACE(s)int main(){FILE *f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,s,10,34,s,34,10,10,34,34,34,34,10,10,10);fclose(f);}%c/*c1*/%cMAIN()%c"
+#define MAIN() GRACE(STRING)
+#define GRACE(s)int main(){FILE *f=fopen("Grace_kid.c","w");fprintf(f,s,10,34,s,34,10,10,34,34,34,34,10,10,10);fclose(f);}
+/*c1*/
+MAIN()
